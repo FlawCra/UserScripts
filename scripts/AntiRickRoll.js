e5e86765e31a7a01f61d444db0b789d83e7df130
Xanax <moritz.fuessel@flawcra.cc>
2022-04-01 22:02:04 +0200

    Added video blocking while fetching dataset

--- scripts/AntiRickRoll.js
+++ scripts/AntiRickRoll.js
@@ -7,19 +7,40 @@
 // @grant       GM_getValue
 // @grant       GM_setValue
 // @grant       unsafeWindow
-// @version     1.0.6-GitHub
+// @version     1.0.7-GitHub
 // @author      FlawCra
 // @license     Apache License 2.0
 // @description Never gonna rickroll you, never gonna let you get rickrolled.
 // @run-at      document-start
 // ==/UserScript==
 
-(() => {
-    let re = new RegExp('(.*)antirickroll.flawcra.cc/(.*)');
-    if(re.test(location.href)) {
-      unsafeWindow.GM_getValue = GM_getValue;
-      unsafeWindow.GM_setValue = GM_setValue;
+function blockVideo(block) {
+  window["antirick_block"] = block;
+  if(!block) return;
+  window["antirick_loop"] = setInterval(() => {
+	  var elements = document.querySelectorAll("video");
+    if(elements.length > 0) {
+      for(const el of elements) {
+        el.style.display = window["antirick_block"] ? "none" : "";
+        el.muted = window["antirick_block"];
+        if(!window["antirick_block"]) {
+          el.currentTime = 0;
+        }
+      }
     }
+
+    if(!window["antirick_block"]) clearInterval(window["antirick_loop"]);
+  }, 250);
+}
+
+(() => {
+    unsafeWindow.GM_getValue = GM_getValue;
+    unsafeWindow.GM_setValue = GM_setValue;
+    unsafeWindow.blockVideo = blockVideo;
+    //if(!GM_getValue("bypassed")) {
+      blockVideo(true);
+    //}
+    
   
     let blocked_ids = [
         "dQw4w9WgXcQ",
@@ -40,11 +61,15 @@
             blocked_ids.push(rr);
         }
         if(blocked_ids.find(i => location.href.includes(i)) && !location.href.includes("https://antirickroll.flawcra.cc/?")) {
-              console.log(GM_getValue("bypassed"))
-              if(!GM_getValue("bypassed")) {
-                location = "https://antirickroll.flawcra.cc/?"+location.href;
-              }
-        GM_setValue("bypassed", false)
-      }
-    }).catch(er => console.log('error',er));
+          console.log(GM_getValue("bypassed"))
+          if(!GM_getValue("bypassed")) {
+            location = "https://antirickroll.flawcra.cc/?"+location.href;
+          }
+          GM_setValue("bypassed", false)
+        }
+        blockVideo(false);
+    }).catch(er => {
+      console.log('error',er);
+      blockVideo(false);
+    });
 })();
