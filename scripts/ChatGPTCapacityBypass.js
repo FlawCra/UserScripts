353b9a7cc10963ff585f76ed4866c7d7f7fb9593
Xanax-C-137 <moritz.fuessel@flawcra.cc>
2023-02-08 10:35:47 +0100

    Update ChatGPTCapacityBypass.js

--- scripts/ChatGPTCapacityBypass.js
+++ scripts/ChatGPTCapacityBypass.js
@@ -3,7 +3,7 @@
 // @namespace   https://flawcra.cc/
 // @match       https://chat.openai.com/auth/login
 // @grant       none
-// @version     1.0.0
+// @version     1.0.0-GitHub
 // @author      FlawCra
 // @license     Apache License 2.0
 // @description A simple script to automatically bypass the "ChatGPT is at Capacity" screen.
