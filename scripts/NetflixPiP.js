fbdc98b7366cf5057dbdb961aa7cbbf1b6e93d80
Xanax-C-137 <moritz.fuessel@flawcra.cc>
2023-03-06 13:35:44 +0100

    Rebranded to Super PiP in regards to multi site support

--- scripts/NetflixPiP.js
+++ scripts/NetflixPiP.js
@@ -1,16 +1,17 @@
 // ==UserScript==
-// @name        Netflix PiP
+// @name        Super PiP
 // @namespace   https://flawcra.cc/
-// @match       https://*netflix.com/*
+// @match       *://*/*
 // @grant       none
-// @version     1.0.1-GitHub
+// @version     1.0.2-GitHub
 // @author      FlawCra
 // @license     Apache License 2.0
-// @description A simple script to add a Picture in Picture button to Netflix.
+// @icon    data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGZpbGw9Im5vbmUiIHZpZXdCb3g9IjAgMCAyNCAyNCIgc3Ryb2tlLXdpZHRoPSIxLjUiIHN0cm9rZT0iY3VycmVudENvbG9yIj4KICA8cGF0aCBzdHJva2UtbGluZWNhcD0icm91bmQiIHN0cm9rZS1saW5lam9pbj0icm91bmQiIGQ9Ik00LjUgNC41bDE1IDE1bTAgMFY4LjI1bTAgMTEuMjVIOC4yNSIgLz4KPC9zdmc+
+// @description A simple script to add a Picture in Picture button to multiple Sites.
 // ==/UserScript==
 
-setInterval(() => {
-    if(document.querySelector("#popout-btn")) return;
+matchDomain(`https:\/\/(.*)\.?netflix\.com(.*)`, () => loop(() => {
+	if(document.querySelector("#popout-btn")) return;
     let data = {};
     data.masterDiv = document.querySelector(`[data-uia="controls-standard"] > div > div:nth-child(3) > div > div:nth-child(3)`)
     if(!data.masterDiv) return;
@@ -69,4 +70,79 @@ setInterval(() => {
 
     data.masterDiv.appendChild(data.spacer);
     data.masterDiv.appendChild(data.icon);
-}, 1);
+}));
+
+matchDomain(`https:\/\/(.*)\.?youtube\.com(.*)`, () => loop(() => {
+	const elem = document.querySelector(".ytp-pip-button");
+  if(elem.style.display == "") return;
+
+  elem.style.display = "";
+}));
+
+
+
+
+
+
+function loop(func) {
+	setInterval(() => {
+		func();
+	}, 1);
+}
+
+function matchDomain(domains, cb) {
+	const url = location.href;
+	if (typeof domains === 'string') { domains = [domains]; }
+	if (domains.some(domain => new RegExp(domain).test(url))) {
+		cb();
+	}
+}
+
+function waitDOMElement(selector, tagName = '', callback, multiple = false) {
+	new window.MutationObserver(function (mutations) {
+		for (const mutation of mutations) {
+			for (const node of mutation.addedNodes) {
+				if (!tagName || (node.tagName === tagName)) {
+					if (node.matches(selector)) {
+						callback(node);
+						if (!multiple) { this.disconnect(); }
+					}
+				}
+			}
+		}
+	}).observe(document, {
+		subtree: true,
+		childList: true
+	});
+}
+
+function removeDOMElement(...elements) {
+	for (const element of elements) {
+		if (element) { element.remove(); }
+	}
+}
+
+function removeClassesByPrefix(el, prefix) {
+	for (const clazz of el.classList) {
+		if (clazz.startsWith(prefix)) {
+			el.classList.remove(clazz);
+		}
+	}
+}
+
+function blockElement(selector, blockAlways = false) {
+	new window.MutationObserver(function (mutations) {
+		for (const mutation of mutations) {
+			for (const node of mutation.addedNodes) {
+				if (node instanceof window.HTMLElement) {
+					if (node.matches(selector)) {
+						removeDOMElement(node);
+						if (!blockAlways) {
+							this.disconnect(); // Stop watching for element being added after one removal
+						}
+					}
+				}
+			}
+		}
+	}).observe(document, { subtree: true, childList: true });
+}
